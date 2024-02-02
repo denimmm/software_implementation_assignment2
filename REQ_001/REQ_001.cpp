589d0bc6b04ad32f677097aefab260805aa1bc5f
Denim MacDougall <dmacdougall7987@conestogac.on.ca>
2024-02-01 22:58:13 -0500

    added 3 tests each for setLength() an setWidth() to comply with REQ003 and finish task 2 of the assignment.

--- REQ_001/REQ_001.cpp
+++ REQ_001/REQ_001.cpp
@@ -2,7 +2,9 @@
 #include "CppUnitTest.h"
 
 extern "C" int getPerimeter(int, int);
-
+extern "C" int getArea(int, int);
+extern "C" void setLength(int, int*);
+extern "C" void setWidth(int, int*);
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace REQ001
@@ -11,11 +13,69 @@ namespace REQ001
 	{
 	public:
 		
-		TEST_METHOD(getPerimeterTEST)
+		TEST_METHOD(getPerimeterTest)
 		{
 			int expected = 24;
 			int result = getPerimeter(5, 7);
 			Assert::AreEqual(expected, result);
 		}
-	};	
+		TEST_METHOD(getAreaTest)
+		{
+			int expected = 35;
+			int result = getArea(5, 7);
+			Assert::AreEqual(expected, result);
+		}
+	};
 }
+
+namespace REQ003 //Both the length and width of the rectangle is to be limited to the range of values 1 to 99, inclusive
+{
+	TEST_CLASS(REQ003)
+	{
+	public:
+		TEST_METHOD(setLengthTest)
+		{
+			int expected = 24;
+			int result = 0;
+			setLength(expected, &result);
+			Assert::AreEqual(expected, result);
+		}
+		TEST_METHOD(setLengthTestMinimum)
+		{
+			int expected = 0;
+			int result = 0;
+			setLength(expected, &result);
+			Assert::AreNotEqual(expected, result);
+		}
+		TEST_METHOD(setLengthTestMaximum)
+		{
+			int expected = 100;
+			int result = 0;
+			setLength(expected, &result);
+			Assert::AreNotEqual(expected, result);
+		}
+
+		TEST_METHOD(setWidthTest)
+		{
+			int expected = 24;
+			int result = 0;
+			setWidth(expected, &result);
+			Assert::AreEqual(expected, result);
+		}
+		TEST_METHOD(setWidthTestMinimum)
+		{
+			int expected = 0;
+			int result = 4;
+			setWidth(expected, &result);
+			Assert::AreNotEqual(expected, result);
+		}
+		TEST_METHOD(setWidthTestMaximum)
+		{
+			int expected = 100;
+			int result = 0;
+			setWidth(expected, &result);
+			Assert::AreNotEqual(expected, result);
+		}
+	};
+}
+////////move it all to another test file? dont forget to commit and push
